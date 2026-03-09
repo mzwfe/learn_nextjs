2b321b1f66e17c99a71986852d59f1592af7bfec
miaozhiwen <miaozhiwen@gdcattsoft.com>
2026-03-09 17:38:40 +0800

    feat: 学习至第五章节, 成功创建页面以及共享UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { inter } from "./ui/fonts";
+import "./ui/global.css";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
+      {/* <body>{children}</body> */}
     </html>
   );
 }
